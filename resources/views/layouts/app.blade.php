c9e9aa5f318fb3f318059140ee444415990c3fc9
Umidjon017 <81345606+Umidjon017@users.noreply.github.com>
2022-05-17 18:27:40 +0500

    front & back ends are ready

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,8 @@
     <body class="font-sans antialiased">
         <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
+{{--        <div class="min-h-screen bg-gray-100">--}}
+        <div class="">
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
